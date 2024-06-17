d2f7896022a7a27088b29ba5a44a18cdd3974709
Zoom <egor147536987@gmail.com>
2024-06-18 01:55:49 +0300

    "Update BASE_URL to https://todo-backend-pwzq.onrender.com and remove BASE_URL_CDN"

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,1 @@
-export const BASE_URL = 'http://localhost:5000'
-export const BASE_URL_CDN = 'http://localhost:5000'
-// export const BASE_URL_CDN = 'https://nest-ecommerse.onrender.com'
+export const BASE_URL = 'https://todo-backend-pwzq.onrender.com'
