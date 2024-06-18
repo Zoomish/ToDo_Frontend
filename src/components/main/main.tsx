c81ead8a74df120164057fe1f3ca9140a898b939
Zoom <egor147536987@gmail.com>
2024-06-18 15:10:11 +0300

    "Removed useContext and userAPI imports, and corresponding code references in Main component"

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
-import React, { useState, useEffect, FC, useContext } from 'react'
+import React, { useState, useEffect, FC } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Layout } from 'antd'
 import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
 import fullscreenIcon from '../../assets/images/fullscreen.svg'
 import Autorization from '../../pages/autorization/autorization'
-import { ECountry, TUser } from '../../utils/typesFromBackend'
+import { ECountry } from '../../utils/typesFromBackend'
 import NotFound from '../../pages/not-found/not-found'
 import { useTranslation } from 'react-i18next'
 import { NotificationProvider } from '../notification-provider/notification-provider'
@@ -21,8 +21,6 @@ import Admins from '../../pages/categories/categories'
 import AddAdmin from '../../pages/add-category/add-category'
 import Admin from '../../pages/category/category'
 import Dark from '../dark/dark'
-import { NotificationContext } from '../../components/notification-provider/notification-provider'
-import * as userAPI from '../../utils/api/user-api'
 import { Footer } from 'antd/es/layout/layout'
 
 const { Header, Sider, Content } = Layout
@@ -41,9 +39,7 @@ const Main: FC<IMain> = ({ token, pathRest, setToken }) => {
   const [dark, setDark] = useState<boolean>(
     localStorage.getItem('dark') === 'true' ?? false
   )
-  const [user, setUser] = useState<TUser>()
   const { t } = useTranslation()
-  const { openNotification } = useContext(NotificationContext)
 
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unused-vars
   const changeLanguage = (lng: ECountry) => {
@@ -59,15 +55,8 @@ const Main: FC<IMain> = ({ token, pathRest, setToken }) => {
   React.useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     i18n.changeLanguage(language)
-    userAPI
-      .getUser(token, +'1')
-      .then((res: TUser) => {
-        setUser(res)
-      })
-      .catch((e: any) => openNotification(e, 'topRight'))
   }, [])
   const [collapse, setCollapse] = useState(false)
-  console.log(user)
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
