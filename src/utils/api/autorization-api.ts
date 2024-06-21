c96da36ce4af02a3eb8aa454acaadc8e0a50705d
Zoom <egor147536987@gmail.com>
2024-06-21 17:51:10 +0300

    "Removed useTelegram hook from autorization and registration pages, moved tg.sendData calls to autorization-api and registration-api"

--- src/utils/api/autorization-api.ts
+++ src/utils/api/autorization-api.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { useTelegram } from '../../services/hooks/use-telegram'
 import { BASE_URL } from '../const'
 import { handleResponse } from '../helpers'
 
+const { tg } = useTelegram()
+
 export const autorization = async (button: any) => {
+  tg.sendData(
+    JSON.stringify(Object.assign({ operation: 'autorization' }, button))
+  )
+
   return await fetch(`${BASE_URL}/auth/login`, {
     method: 'POST',
     headers: {
