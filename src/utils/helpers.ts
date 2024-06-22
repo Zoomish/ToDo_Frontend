1224086f08c066ec71fefcf274d443811047f70a
Zoom <egor147536987@gmail.com>
2024-06-22 15:56:09 +0300

    "Added import and usage of useTelegram hook in handleResponse function"

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,10 +3,22 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+
+import { useTelegram } from '../services/hooks/use-telegram'
+
+const { tg } = useTelegram()
 export const handleResponse = async (response: Response) => {
   if (response.ok) return await response.json()
   else if (response.status === 401) {
     localStorage.removeItem('token')
     location.reload()
+    tg.sendData(
+      JSON.stringify(
+        Object.assign({
+          operation: 'badtoken',
+          tg_id: tg.initDataUnsafe.user.id
+        })
+      )
+    )
   } else return await Promise.reject(response.status)
 }
