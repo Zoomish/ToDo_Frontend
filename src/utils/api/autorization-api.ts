f771a2b079bec2f708cae94a289db5a058f73959
Zoom <egor147536987@gmail.com>
2024-06-22 14:06:47 +0300

    "Moved Telegram API calls from autorization-api and registration-api to autorization and registration components"

--- src/utils/api/autorization-api.ts
+++ src/utils/api/autorization-api.ts
@@ -1,13 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { useTelegram } from '../../services/hooks/use-telegram'
 import { BASE_URL } from '../const'
 import { handleResponse } from '../helpers'
-
-const { tg } = useTelegram()
-
 export const autorization = async (button: any) => {
-  await fetch(`${BASE_URL}/auth/login`, {
+  return await fetch(`${BASE_URL}/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -16,8 +12,4 @@ export const autorization = async (button: any) => {
       ...button
     })
   }).then(async (res) => await handleResponse(res))
-
-  return tg.sendData(
-    JSON.stringify(Object.assign({ operation: 'autorization' }, button))
-  )
 }
