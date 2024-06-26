0ef90d42714a0abfc55e98652113c310dcb1a267
Zoom <egor147536987@gmail.com>
2024-06-26 18:57:56 +0300

    "Removed location.reload() from handleResponse in helpers.ts and added it to Autorization component"

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,7 +7,6 @@ export const handleResponse = async (response: Response) => {
   if (response.ok) return await response.json()
   else if (response.status === 401) {
     localStorage.removeItem('token')
-    location.reload()
     return await Promise.reject(response.status)
   } else return await Promise.reject(response.status)
 }
